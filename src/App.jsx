130826c75068f477590803b16e60dfeaa8b401c2
Seth Gaglione <seth.gaglione@live.com>
2021-10-11 16:02:47 -0500

    Fixed Warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
 import Header from './components/Header';
-import About from './components/About'
+//import About from './components/About'
 
 /**
  * Top level of App to organize components
