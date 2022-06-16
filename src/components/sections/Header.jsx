e6191f011d26c4d0b82f48735abea8780bb3800e
Seth Gaglione <seth.gaglione@live.com>
2022-06-16 16:41:36 -0500

    Added thank you message

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -8,19 +8,20 @@ const Header = () => (
   <div className="header">
     <div className="content">
       <div className="header-text">
-        <h1>Hacklahoma 2022</h1> 
+        <h1>Hacklahoma 2022</h1>
         <div className="subcontent">
           <p>February 12 - 13, 2022</p>
           <p className="subtitle">University Of Oklahoma</p>
         </div>
         <div className="registration">
-          <a href="https://register.hacklahoma.org/">Register Now!</a>
+          Thank you for attending Hacklahoma 2022!
+          {/*<a href="https://register.hacklahoma.org/">Register Now!</a>*/}
         </div>
       </div>
 
       {/* <a href="/" className="register"><p>Register</p></a> */}
-      <div className="header-logo">{}</div>
-    </div> 
+      <div className="header-logo">{ }</div>
+    </div>
   </div>
 );
 
